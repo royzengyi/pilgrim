f0bed1837e293c72712a13899982ed6cfa52f1f1
zengyi <zengyi@xmqvip.com>
2020-01-06 15:25:13 +0800

    add gradle plugin

---
+++ plugin/src/main/groovy/com/royzeng/plugin/TimeCostLogPlugin.groovy
@@ -0,0 +1,14 @@
+package com.royzeng.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TimeCostLogPlugin implements Plugin<Project>{
+
+    @Override
+    void apply(Project project) {
+        System.out.println("============================")
+        System.out.println("自定义Gradle插件开始")
+        System.out.println("============================")
+    }
+}
